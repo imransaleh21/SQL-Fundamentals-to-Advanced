4493a48afa572e72a14bf5d42d7061e000bd5b17
imransaleh21 <imranruet88@gmail.com>
2025-07-21 12:24:28 +0600

    Practice LEFT, RIGHT and FULL Join

--- SQL-SELECT_Quesries/7_JOIN.sql
+++ SQL-SELECT_Quesries/7_JOIN.sql
@@ -8,3 +8,21 @@ SELECT customers.id, first_name, Country, score
 FROM customers 
 INNER JOIN orders
 ON id = customer_id
+
+---LEFT JOIN---
+SELECT id, first_name, country, order_id, order_date
+FROM customers
+LEFT JOIN orders
+ON customers.id = orders.customer_id;
+
+---RIGHT JOIN---
+SELECT order_id, customer_id, sales, first_name, score
+FROM customers
+RIGHT JOIN orders
+ON customers.id = orders.customer_id;
+
+---FULL JOIN---
+SELECT customers.id, order_id, sales, Country, score
+FROM customers 
+FULL JOIN orders
+ON id = customer_id
